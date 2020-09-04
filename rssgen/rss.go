95a3fe1579af148750f9b522989694a3f01c4e8b
skuzzymiglet <skuzzymiglet@gmail.com>
2020-09-04 11:39:04 +0100

    A real RSS feed

--- rssgen/rss.go
+++ rssgen/rss.go
@@ -11,12 +11,19 @@ import (
 // Generate a rss-like file and return the content as byte
 func Generate(seed int64, size int64) (rssContent []byte) {
 	gofakeit.Seed(seed)
-	var rss = RSS{
+	timeFmt := time.RFC822Z
+	var rss = &RSS{
+		Version: "2.0",
 		Channel: ChannelStruct{
-			Item: make([]Item, size),
+			Item:          make([]Item, size),
+			LastBuildDate: gofakeit.Date().Format(timeFmt),
+			PubDate:       gofakeit.Date().Format(timeFmt),
 		},
 	}
 	gofakeit.Struct(&rss)
+	for i := range rss.Channel.Item {
+		rss.Channel.Item[i].PubDate = gofakeit.Date().Format(timeFmt)
+	}
 	rssContent, err := xml.Marshal(&rss)
 	if err != nil {
 		log.Println(err)
@@ -25,23 +32,25 @@ func Generate(seed int64, size int64) (rssContent []byte) {
 }
 
 type RSS struct {
-	XMLName xml.Name `xml:"rss"`
-	Channel ChannelStruct
+	XMLName xml.Name      `xml:"rss"`
+	Version string        `fake:"skip" xml:"version,attr"`
+	Channel ChannelStruct `xml:"channel"`
 }
 
 type Item struct {
-	Title       string    `fake:"{sentence:3}"`
-	PubDate     time.Time `fake:"{date}"`
-	Link        string    `fake:"{url}"`
-	Category    string    `fake:"{bs}"`
-	Description string    `fake:"{loremipsumparagraph}"`
+	XMLName     xml.Name `xml:"item"`
+	Title       string   `xml:"title" fake:{sentence:3}`
+	PubDate     string   `fake:"skip" xml:"pubDate"`
+	Link        string   `fake:"{url}" xml:"link"`
+	Category    string   `fake:"{bs}" xml:"category"`
+	Description string   `fake:"{loremipsumparagraph}" xml:"description"`
 }
 
 type ChannelStruct struct {
-	Title         string    `fake:"{bs}"`
-	Description   string    `fake:"{sentence:8}"`
-	Link          string    `fake:"{url}"`
-	PubDate       time.Time `fake:"{date}"`
-	LastBuildDate time.Time `fake:"{date}"`
+	Title         string `fake:"{bs}" xml:"title"`
+	Description   string `fake:"{sentence:8}" xml:"description"`
+	Link          string `fake:"{url}" xml:"link"`
+	PubDate       string `fake:"skip" xml:"pubDate"`
+	LastBuildDate string `fake:"skip" xml:"lastBuildDate"`
 	Item          []Item
 }
